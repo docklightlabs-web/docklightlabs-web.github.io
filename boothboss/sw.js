07bd04fc4414b4f001894de49ef7f2a0e3de35b1
docklightlabs-web <docklightlabs-web@users.noreply.github.com>
2026-09-17 16:12:42 -0400

    Update BoothBoss onboarding tools

--- boothboss/sw.js
+++ boothboss/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='boothboss-shell-v1';
+const CACHE='boothboss-shell-v2';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon-192.svg','./assets/icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
