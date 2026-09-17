6f3f36dd0f93c9dce7f1e5db82b564d4ad8ef04e
DockLight Labs <docklightlabs-web@users.noreply.github.com>
2026-09-17 15:49:09 -0400

    Publish BoothBoss MVP

---
+++ boothboss/sw.js
@@ -0,0 +1,6 @@
+'use strict';
+const CACHE='boothboss-shell-v1';
+const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon-192.svg','./assets/icon-512.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));});
